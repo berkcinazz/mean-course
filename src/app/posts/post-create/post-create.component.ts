41761f52dc69d70d847962167ac7c4cc6d343dc7
berkcinazz <berk.cinaz@gmail.com>
2022-12-24 17:27:47 +0300

    (feat) : mime tpye validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { mimeType } from '../../validators/mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -18,6 +19,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
 
   constructor(
     public postsService: PostsService,
@@ -32,6 +34,10 @@ export class PostCreateComponent implements OnInit {
       }),
       content: new FormControl(null, {
         validators: [Validators.required],
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators : [mimeType]
       }),
     });
 
@@ -74,5 +80,15 @@ export class PostCreateComponent implements OnInit {
       );
     }
     this.form.reset();
+  }
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 }
